c2ecb26e51299fc81cb40a6630fc7570cc189347
Orlov Egor <dbn.orlov@gmail.com>
2022-01-10 21:16:30 +0300

    add utils json list task

---
+++ app/src/main/java/ru/orlovegor/diaryapp/utils/JsonListTask.kt
@@ -0,0 +1,32 @@
+package ru.orlovegor.diaryapp.utils
+
+/*даты и время тасков
+    id1 = 20.01 10.00 - 11.00б,
+    id2 = 21.01 12.30 - 13.00,
+    id3 22.01 13.00 - 15.00*/
+
+val jsonListTask = """ 
+        [
+         {
+            "id": 1,
+            "date_start": "1642672815",
+            "date_finish": "1642676775",
+            "name": "buy foods",
+            "description": "Meat,eggs,bread"
+         },
+         {
+             "id": 2,
+             "date_start": "1642768215",
+             "date_finish": "1642770015",
+             "name": "cut a hair",
+             "description": "very beautiful"
+         },
+         {
+             "id": 3,
+             "date_start": "1642856415",
+             "date_finish": "1642863615",
+             "name": "play games",
+             "description": "WOW Dota2, Csgo"
+    }
+    ]
+    """
